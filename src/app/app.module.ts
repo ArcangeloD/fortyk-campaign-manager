f1aecc03503ad8a5b61e0a2cf714fd846066c54f
ArcangeloD <arcangelo.daniaux@outlook.com>
2021-05-19 15:54:27 +0200

    Development start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { NavComponent } from './nav/nav.component';
 
 //bootsrap modules
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewsComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -23,8 +30,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
-    NgbModule
-    //bootstrap modules
+    NgbModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'news', component: NewsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
