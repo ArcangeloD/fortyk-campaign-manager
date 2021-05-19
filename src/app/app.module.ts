4e867ee149269620d1beef68f54d3f6317b019b7
ArcangeloD <arcangelo.daniaux@outlook.com>
2021-05-19 14:18:43 +0200

    Replace material by bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+//bootsrap modules
+
 
 @NgModule({
   declarations: [
@@ -18,7 +22,9 @@ import { environment } from '../environments/environment';
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    NgbModule
+    //bootstrap modules
   ],
   providers: [],
   bootstrap: [AppComponent]
