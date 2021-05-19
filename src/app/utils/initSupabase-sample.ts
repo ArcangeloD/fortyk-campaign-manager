6015885dd592067507baae704454af575b1a5ff3
ArcangeloD <arcangelo.daniaux@outlook.com>
2021-05-19 10:25:26 +0200

    Create initSupabase-sample.ts

---
+++ src/app/utils/initSupabase-sample.ts
@@ -0,0 +1,5 @@
+// Rename this file as initSupabase.ts and replace the placeholders with your supabase url and api key
+export const initSupabase = {
+  url = '<supabase url>',
+  key = '<supabase api key>'
+};
